5f50ff7bf12fd16a4ff850326e5fbfb8de54e8dd
Yuri Bukatkin <yurij-bukatkin@yandex.ru>
2017-11-21 02:23:28 +0300

    Add ExistKV func in cache driver

--- src/shkaff/drivers/cache/cache.go
+++ src/shkaff/drivers/cache/cache.go
@@ -2,24 +2,25 @@ package cache
 
 import (
 	"errors"
-	"strings"
 	"fmt"
 	"log"
 	"os"
+	"strings"
+
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
 const (
-	CACHEPATH = "./cache/cache.db" 
+	CACHEPATH = "./cache/cache.db"
 )
 
-type Cache struct{
+type Cache struct {
 	*leveldb.DB
 }
 
-func InitCacheDB() (cache *Cache){
+func InitCacheDB() (cache *Cache) {
 	err := os.MkdirAll(CACHEPATH, 0644)
-	if err!=nil{
+	if err != nil {
 		log.Fatalln(err)
 	}
 	cache = new(Cache)
@@ -32,40 +33,48 @@ func InitCacheDB() (cache *Cache){
 
 }
 
-func (cache *Cache) SetKV(userId, campId int, table, sheet string) (err error){
-	key := []byte(fmt.Sprintf("%d|%d", userId, campId))
+func (cache *Cache) SetKV(userID, campID int, table, sheet string) (err error) {
+	key := []byte(fmt.Sprintf("%d|%d", userID, campID))
 	value := []byte(fmt.Sprintf("%s|%s", table, sheet))
 	err = cache.DB.Put(key, value, nil)
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 	return
 }
 
-func (cache *Cache) GetKV(userId, campId int) (table, sheet string, err error){
+func (cache *Cache) GetKV(userID, campID int) (table, sheet string, err error) {
 	var value []byte
-	key := []byte(fmt.Sprintf("%d|%d", userId, campId))
+	key := []byte(fmt.Sprintf("%d|%d", userID, campID))
 	value, err = cache.DB.Get(key, nil)
-	if err!=nil{
-		return "","", err
+	if err != nil {
+		return "", "", err
 	}
 	valueStr := string(value[:])
-	val:= strings.Split(valueStr, "|")
-	if len(val) != 2{
+	val := strings.Split(valueStr, "|")
+	if len(val) != 2 {
 		log.Printf("Value %s in key %s is bad\n", valueStr, key)
 		cache.Delete(key, nil)
-		return "","", errors.New("Bad key-value")
+		return "", "", errors.New("Bad key-value")
 	}
 	table = val[0]
 	sheet = val[1]
 	return table, sheet, nil
 }
 
-func (cache *Cache) DeleteKV(userId, campId int) (err error){
-	key := []byte(fmt.Sprintf("%d|%d",userId, campId))	
+func (cache *Cache) DeleteKV(userID, campID int) (err error) {
+	key := []byte(fmt.Sprintf("%d|%d", userID, campID))
 	err = cache.DB.Delete(key, nil)
-	if err!=nil{
+	if err != nil {
 		return err
 	}
 	return
+}
+
+func (cache *Cache) ExistKV(userID, campID int) (result bool, err error) {
+	key := []byte(fmt.Sprintf("%d|%d", userID, campID))
+	if res, err := cache.Get(key, nil); res == nil && err != nil {
+		return false, err
+	}
+	return true, nil
 }
